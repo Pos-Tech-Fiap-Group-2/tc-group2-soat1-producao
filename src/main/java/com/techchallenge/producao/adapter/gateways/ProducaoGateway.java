9971ade1548e0463f577cff2840ed003038fcb47
Danilo Monteiro <dmonteiro.souza@gmail.com>
2024-01-11 20:20:07 -0300

    Revert "Feature/Disparar status para API de pedidos"
    This reverts commit 922b532f5fd30dcdeff6999cf1b148978bbfc5b7.

--- src/main/java/com/techchallenge/producao/adapter/gateways/ProducaoGateway.java
+++ src/main/java/com/techchallenge/producao/adapter/gateways/ProducaoGateway.java
@@ -2,14 +2,13 @@ package com.techchallenge.producao.adapter.gateways;
 
 import com.techchallenge.producao.core.domain.entities.Pedido;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface ProducaoGateway {
 
 	void adicionarPedidoAFilaDeProducao(String id);
 
-	void atualizarStatusDePedidoEmProducao(Pedido pedido) throws IOException;
+	void atualizarStatusDePedidoEmProducao(Pedido pedido);
 
 	List<Pedido> consultarFilaDePedidos();
 
